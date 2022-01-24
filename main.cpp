d94132c215e4c84186394dae0d1412793f55075a
daniel-pawelko-ics20 <82897918+daniel-pawelko-ics20@users.noreply.github.com>
2022-01-24 11:55:58 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2022 Daniel Pawelko All rights reserved.
+//
+// Created by: Daniel Pawelko
+// Created on: Jan 2022
+// Max Num
+
+#include <iostream>
+#include <random>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    // main function for max number
+
+    // stuff for random
+    std::random_device rd;
+    std::mt19937 eng(rd());
+    std::uniform_int_distribution<int> distr(1, 100);
+
+    // defining variables
+    int arr[10];
+    int biggest = 0;
+
+    // create array with 10 random numbers/process
+    for (int rand = 0; rand < 10; rand++) {
+        arr[rand] = distr(eng);
+    }
+
+    // add up arr/process
+    for (int num = 0; num < 10; num++) {
+        cout << "The random number " << num + 1 << " is: " << arr[num] << endl;
+        if (arr[num] > biggest) {
+            biggest = arr[num];
+        }
+    }
+
+    // output
+    cout << endl;
+    cout << "Biggest number is " << biggest << endl;
+
+    // output finished
+    cout << endl;
+    cout << "Done." << endl;
+}
